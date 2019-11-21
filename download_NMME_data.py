9a0da6dcf08a081c2a6f302e0309f743465aba02
Sonja Totz <sonja.totz@gmail.com>
2019-11-21 17:32:44 -0500

    Time is correct in netCDF output, use only xarrays

---
+++ download_NMME_data.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 09:45:58 2019
+
+@author: sonja totz
+"""
+
+from datetime import datetime
+import xarray as xa
+
+class NMME():
+    """ class to download data from NMME models"""
+    
+    def __init__(self,model,url,var):
+        """ Initialize model, variable, url for download"""
+        self.model = model
+        self.url = url
+        self.var = var
+        self.fullURL = f"{self.url}/.{self.var}/dods"
+
+    def download_data(self,output_label,forecast_reference_time,forecastMonths):
+        """ 
+        download data for all realization. Forecast Start Time (forecast_reference_time),
+        forecast lead (forecast_lead) and months 
+        (forecast_months) is specified by user
+        """
+        print(f"Access {self.var} from model {self.model} via {self.fullURL}")
+        with xa.open_dataset(self.fullURL, decode_times=False)  as self.nc:
+            #  Check for variable in dataset 
+            assert self.var in (self.nc.variables),"Variable not found!"
+            
+            # netCDF variable of interest is: 
+            #
+            # float var(S, M, L, Y, X)
+            #   where
+            #     S : Forecast Start Time (forecast_reference_time)
+            #     M : Ensemble Member (realization)
+            #     L : Lead (forecast_period)
+            #     Y : Latitude (latitude)
+            #     X : Longitude (longitude)
+            #
+            self.m = int(self._date_to_num(forecast_reference_time))
+            self.ds = self.nc.sel(S=self.m,L=forecastMonths)
+            self.forecast_time = [self._num_to_date(self.m + i) for i in (forecastMonths)]
+            self.ds = self.ds.rename({'X': 'longitude','Y': 'latitude'})
+            self.ds = (self.ds.assign_coords(L=self.forecast_time))
+            self.ds.to_netcdf(path=output_label, engine='scipy')
+
+    def _num_to_date(self,nMonths):
+        """ Helper-function to convert number to date"""
+        self.nMonths = int(nMonths)
+        self.baseDate = datetime(1960,1,1,0,0,0)
+        self.deltayears  = self.nMonths//12
+        self.deltamonths = self.nMonths % 12
+        self.requestedDate = datetime(self.baseDate.year + self.deltayears, self.baseDate.month + self.deltamonths, 1,0,0,0)
+        return self.requestedDate
+
+    def _date_to_num(self,reqDate):
+        """ Helper-function to convert date to number"""
+        self.baseDate = datetime(1960,1,1,0,0,0)
+        self.nMonths  = (reqDate.year - self.baseDate.year) * 12
+        self.nMonths += reqDate.month - self.baseDate.month
+        return self.nMonths
+
+
--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 16 18:43:02 2019
+Created on Thu Nov 21 09:45:58 2019
 
 @author: sonja totz
 """
-from download_NASA_data import NMME
+from download_NMME_data import NMME
 from datetime import datetime
 
 
 if __name__ == '__main__':
     
     URL_PREFIX = "http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME"
-    output_label = "/home/sonja/Documents/NASA/NASA-Models/output/test.nc"
+    output_label = "model_"
+    
     #forecast start date
     forecast_reference_time = datetime(1982,11,1,0,0,0)
-
-    forecastDates = []
-    forecastDates.append(datetime(1982,12,31,0,0,0))
-    forecastDates.append(datetime(1982+1,1,31,0,0,0))
-    forecastDates.append(datetime(1982+1,2,28,0,0,0))
+    # How many months in advance?
+    forecastMonths = [2.5,3.5,4.5]
     #dictionary of all possible NASA models (key: model name, value: model url adress)
     nasa_dict = {
-              "NASA-GMAO" : "/.NASA-GMAO/.MONTHLY",
-#              "NASA-GMAO-062012" : "/.NASA-GMAO-062012/.MONTHLY",
+              "NASA-GMAO" :        "/.NASA-GMAO/.MONTHLY",
+              "NASA-GMAO-062012" : "/.NASA-GMAO-062012/.MONTHLY",
+              "NASA-GEOSS2S":      "/.NASA-GEOSS2S/.HINDCAST/.MONTHLY"
             }
     
     # iterate over all nasa models
     for model,url in nasa_dict.items():
+        output_label_full = output_label + model + ".nc"
         print(model,url)
         var = "prec"
         nasa = NMME(model,URL_PREFIX+url,var)
-        nasa.download_data(output_label,forecast_reference_time,forecastDates)
-        
-        
+        nasa.download_data(output_label_full,forecast_reference_time,forecastMonths)
