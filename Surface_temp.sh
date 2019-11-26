4cfd99428060a1d9d6b339836db3d0bd8beac001
petersherman123 <39100475+petersherman123@users.noreply.github.com>
2019-11-26 12:46:58 -0500

    Update Surface_temp.sh

--- Surface_temp.sh
+++ Surface_temp.sh
@@ -11,12 +11,21 @@
 module load cdo
 module load nco
 
-PATH_DOWNLOAD="/gpfs/fs1/collections/cdg/data/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/TS/"
-PATH_TO_STORE_FILES=""
+set DATA_PATH=(/gpfs/fs1/collections/cdg/data/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/TS)
+set OUTPUT_PATH=(/glade/scratch/petersh)
 
-echo "Download data from ${PATH_DOWNLOAD} to ${PATH_TO_STORE_FILES}"
-for i in {01..35}; do 
-    #Download only Oct-Feb data
-    ncks -d time,0,,12 -d time,1,,12 -d time,8,,12 -d time,9,,12 -d time,10,,12 -d time,11,,12 "${PATH_DOWNLOAD}/b.e11.B20TRC5CNBDRD.f09_g16.0${i}.cam.h.TS.185001-200512.nc" "${PATH_TO_STORE_FILES}/TS_${i}.nc"
-done
-echo "Finished!"
+#Download surface temperature data from the first 35 ensemble members in the CESM Large Ensemble
+foreach i (`seq 01 01 35`)
+    echo ${i}
+    if ($i == 1) then
+	#Download only Oct-Feb data
+        ncks -d time,0,,12 -d time,1,,12 -d time,8,,12 -d time,9,,12 -d time,10,,12 -d time,11,,12 ${DATA_PATH}/b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.TS.185001-200512.nc ${OUTPUT_PATH}/TS_$i.nc
+
+    else if ($i < 10) then
+        ncks -d time,0,,12 -d time,1,,12 -d time,8,,12 -d time,9,,12 -d time,10,,12 -d time,11,,12 ${DATA_PATH}/b.e11.B20TRC5CNBDRD.f09_g16.00$i.cam.h0.TS.192001-200512.nc ${OUTPUT_PATH}/TS_$i.nc
+
+    else
+        ncks -d time,0,,12 -d time,1,,12 -d time,8,,12 -d time,9,,12 -d time,10,,12 -d time,11,,12 ${DATA_PATH}/b.e11.B20TRC5CNBDRD.f09_g16.0$i.cam.h0.TS.192001-200512.nc ${OUTPUT_PATH}/TS_$i.nc
+    endif
+end
+
